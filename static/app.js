f16de83d2cb4570160430a55a2dcbab0ff7db46c
Roberto <rob.riveros.es@gmail.com>
2025-12-18 13:28:10 +0100

    REF: change the default language to english

--- static/app.js
+++ static/app.js
@@ -94,7 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   };
 
-  let lang = localStorage.getItem("lang") || "es";
+  let lang = localStorage.getItem("lang") || "en";
   let theme = localStorage.getItem("theme") || "dark";
 
   const navbar = document.getElementById("navbar");
